450ae5ad869248020f583f6ea74c03e4548d1fbf
Christoph Mewes <christoph@kubermatic.com>
2024-10-03 19:59:04 +0200

    copy of the 'for' variable can be deleted

--- cmd/deck/main.go
+++ cmd/deck/main.go
@@ -1211,9 +1211,6 @@ func handleArtifactView(o options, sg *spyglass.Spyglass, cfg config.Getter) htt
 		var lens *config.LensFileConfig
 		for _, configLens := range cfg().Deck.Spyglass.Lenses {
 			if configLens.Lens.Name == lensName {
-
-				// Directly followed by break, so this is ok
-				// nolint: exportloopref
 				lens = &configLens
 				break
 			}
